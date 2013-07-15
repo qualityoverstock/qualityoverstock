51b8605f1d09a4c7c58bf0435cfd3a70089ded9f
ryanm <ryanm@zoovy.com>
2013-07-15 11:46:51 -0700

    Infinite product list additional items point raised
    This should compensate for the extra footer space and load new product
    when the 2nd to last row is reached.

--- extensions/prodlist_infinite.js
+++ extensions/prodlist_infinite.js
@@ -209,7 +209,7 @@ else if(plObj.prodlist.page_in_focus >= plObj.prodlist.total_page_count)	{
 else	{
 	$(window).on('scroll.infiniteScroll',function(){
 		//will load data when two rows from bottom.
-		if( $(window).scrollTop() >= ( $(document).height() - $(window).height() - ($tag.children().first().height() * 2) ) )	{
+/*QOS*/	if( $(window).scrollTop() >= ( $(document).height() - 475 - $(window).height() - ($tag.children().first().height() * 2) ) )	{
 			if($tag.data('isDispatching') == true)	{}
 			else	{
 				plObj.prodlist.page_in_focus += 1;
