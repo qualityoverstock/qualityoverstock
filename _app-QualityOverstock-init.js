28a01a86c76b1535841616de9fab64520cde56aa
ryanm <ryanm@zoovy.com>
2013-07-24 17:20:14 -0700

    QOS init: AB testing removed from loading / version

--- _app-QualityOverstock-init.js
+++ _app-QualityOverstock-init.js
@@ -20,7 +20,7 @@ app.rq.push(['extension',0,'myRIA','app-quickstart.js','startMyProgram']);
 app.rq.push(['extension',0,'prodlist_infinite','extensions/prodlist_infinite.js']);
 
 //app.rq.push(['extension',1,'google_analytics','extensions/partner_google_analytics.js','startExtension']);
-app.rq.push(['extension',1,'tools_ABtesting','extensions/tools_ABtesting.js']);
+//app.rq.push(['extension',1,'tools_ABtesting','extensions/tools_ABtesting.js']);
 app.rq.push(['extension',0,'partner_addthis','extensions/partner_addthis.js','startExtension']); //adds social media sharing to prodcut page
 //app.rq.push(['extension',1,'resellerratings_survey','extensions/partner_buysafe_guarantee.js','startExtension']); /// !!! needs testing.
 //app.rq.push(['extension',1,'buysafe_guarantee','extensions/partner_buysafe_guarantee.js','startExtension']);
